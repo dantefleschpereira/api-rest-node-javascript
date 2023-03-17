6794619c1fe7f1a8efb0e2d2aad2e228e389a29f
Dante <48799984+dantefleschpereira@users.noreply.github.com>
2023-03-17 15:16:58 -0300

    mongoose update

---
+++ db/conn.js
@@ -0,0 +1,18 @@
+
+const mongoose = require('mongoose')
+
+async function main() {
+    try {
+
+        mongoose.set("strictQuery", true);
+        await mongoose.connect(
+            "mongodb+srv://dantefp:kaODrV883XsDbNpf@cluster0.cyq4j0f.mongodb.net/?retryWrites=true&w=majority"
+        );
+        console.log("Conectado ao banco");
+    } catch (error) {
+        console.log(`Erro: ${error}`);
+    }
+}
+
+module.exports = main
+
